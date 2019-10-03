bdd7fcd8feeaac1a71bb4ccebe04533471b7df98
Scott Gilmore <scott@plsol.com>
2019-10-03 12:53:31 -0400

    Breakout homepage into it's own component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Components
+import Directory from '../../components/directory/directory.component';
+
+/**
+ * Display the store home page.
+ */
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory />
+    </div>
+);
+
+export default HomePage;
