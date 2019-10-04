8f3fbeb59d8b3beb4cdeacf548fc0f2d60897d22
Scott Gilmore <scott@plsol.com>
2019-10-04 13:05:27 -0400

    Add SignInAndSignUp component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import './App.css';
-import './pages/homepage/homepage.styles.scss';
-
 // Components
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
+// Styles
+import './App.css';
+import './pages/homepage/homepage.styles.scss';
 
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Components
+import SignIn from '../../components/sign-in/sign-in.component';
+
+// Styles
+import './sign-in-and-sign-up.styles.scss';
+
+/**
+ * Display the SignIn and SignUp forms.
+ */
+const SignInAndSignUp = () => (
+    <div className='sign-in-and-sign-up'>
+        <SignIn />
+    </div>
+);
+
+export default SignInAndSignUp;
