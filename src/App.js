fe810ca275bde2f82d9030b4ad134ad83ba852cf
Scott Gilmore <scott@plsol.com>
2019-10-04 16:32:31 -0400

    Retrieve current user record from firebase and set current user value

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 // Firebase
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 // Components
 import Header from './components/header/header.component';
@@ -26,7 +26,24 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => this.setState({ currentUser: user }));
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        // Use the DocumentSnapshot to get the current user data.
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            }
+          });
+        });
+      }
+
+      // If userAuth is null (user logged out), ensure the current user value is null.
+      this.setState({ currentUser: userAuth });
+    });
   };
 
   componentWillUnmount() {
