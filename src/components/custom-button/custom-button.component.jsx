8fd02d298dc710dd128ade22581ee25d2d7bc93c
Scott Gilmore <scott@plsol.com>
2019-10-04 14:43:51 -0400

    Add google sign-in button

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -6,10 +6,14 @@ import './custom-button.styles.scss';
 /**
  * Display a custom button.
  * @param {String} children - button title.
+ * @param {Boolean} isGoogleSignIn - whether or not button is Google Sign-In button.
  * @param {Object} otherProps - onClick, type.
  */
-const CustomButton = ({ children, ...otherProps }) => (
-    <button className='custom-button' {...otherProps}>
+const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+    <button
+        className={`custom-button ${isGoogleSignIn ? 'google-sign-in' : ''}`}
+        {...otherProps}
+    >
         {children}
     </button>
 );
