0b4da860fac8e4a0f71e0682f04b15e7d4137b97
Scott Gilmore <scott@plsol.com>
2019-10-07 11:30:07 -0400

    Add redux-persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,22 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+
+// Tell redux-persist to use local storage as default storage.
+import storage from 'redux-persist/lib/storage';
 
 // Reducers
 import cartReducer from './cart/cart.reducer';
 import userReducer from './user/user.reducer';
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart'],
+};
+
+const rootReducer = combineReducers({
     cart: cartReducer,
     user: userReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
