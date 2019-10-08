12f9033b1480b852fbdf32a26a89c525750f17ae
Scott Gilmore <scott@plsol.com>
2019-10-08 13:45:45 -0400

    Fetch shop data from db and add to redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
 
 // Components
 import CollectionPage from '../collection/collection.component';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
+// Firebase
+import { convertCollectionsSnapshotToMap, firestore } from '../../firebase/firebase.utils';
+
+// Redux actions
+import { updateCollections } from '../../redux/shop/shop.actions';
+
 /**
- * Display the shop page.
+ * Display and manage the shop page.
  * This component renders the CollectionOverview and CollectionPage components.
  */
-const ShopPage = ({ match }) => (
-    <div className='shop-page'>
-        <Route exact path={ `${ match.path }` } component={ CollectionsOverview } />
-        <Route path={ `${ match.path }/:collectionId` } component={ CollectionPage } />
-    </div>
-);
+class ShopPage extends Component {
+    unsubscribeFromSnapshot = null;
 
-export default ShopPage;
+    componentDidMount() {
+        const { updateCollections } = this.props;
+        const collectionRef = firestore.collection('collections');
+
+        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(snapshot => {
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+            console.log('cllectionsMap', collectionsMap);
+            updateCollections(collectionsMap);
+        });
+    };
+
+    render() {
+        const { match } = this.props;
+
+        return (
+            <div className='shop-page'>
+                <Route exact path={ `${ match.path }` } component={ CollectionsOverview } />
+                <Route path={ `${ match.path }/:collectionId` } component={ CollectionPage } />
+            </div>
+        )
+    };
+};
+
+const mapDispatchToProps = dispatch => ({
+    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
