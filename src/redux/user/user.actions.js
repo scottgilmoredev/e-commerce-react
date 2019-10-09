1b2bbb8c33664427f3e27c0313aa9c744f2939e6
Scott Gilmore <scott@plsol.com>
2019-10-09 15:40:18 -0400

    Implement user saga pattern

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,52 @@
-import { UserActionTypes } from './user.types';
+import UserActionTypes from './user.types';
 
-export const setCurrentUser = user => ({
+export const checkUserSession = () => ({
+    type: UserActionTypes.CHECK_USER_SESSION,
+});
+
+export const emailSignInStart = emailAndPassword => ({
+    payload: emailAndPassword,
+    type: UserActionTypes.EMAIL_SIGN_IN_START,
+});
+
+export const googleSignInStart = () => ({
+    type: UserActionTypes.GOOGLE_SIGN_IN_START,
+});
+
+export const signInFailure = error => ({
+    payload: error,
+    type: UserActionTypes.SIGN_IN_FAILURE,
+});
+
+export const signInSuccess = user => ({
     payload: user,
-    type: UserActionTypes.SET_CURRENT_USER,
+    type: UserActionTypes.SIGN_IN_SUCCESS,
+});
+
+export const signOutFailure = error => ({
+    payload: error,
+    type: UserActionTypes.SIGN_OUT_FAILURE,
+});
+
+export const signOutStart = () => ({
+    type: UserActionTypes.SIGN_OUT_START,
+});
+
+export const signOutSuccess = () => ({
+    type: UserActionTypes.SIGN_OUT_SUCCESS,
+});
+
+export const signUpFailure = error => ({
+    payload: error,
+    type: UserActionTypes.SIGN_UP_FAILURE,
+});
+
+export const signUpStart = userCredentials => ({
+    payload: userCredentials,
+    type: UserActionTypes.SIGN_UP_START,
+});
+
+export const signUpSuccess = ({ user, additionalData }) => ({
+    payload: { user, additionalData },
+    type: UserActionTypes.SIGN_UP_SUCCESS,
 });
