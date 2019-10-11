ada6cadd13c87a5c2ce337d23f7c62f297027d9f
Scott Gilmore <scott@plsol.com>
2019-10-11 15:04:38 -0400

    Add express server to handle backend stripe payments and move frontend code to client folder

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_SmMcunW9gjRfMJyVq2414gSg00ArMVpueO';
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful');
+        axios({
+            data: {
+                amount: priceForStripe,
+                token: token
+            },
+            method: 'post',
+            url: 'payment',
+        })
+        .then(response => {
+            alert('succesful payment');
+        })
+        .catch(error => {
+            console.log('Payment Error: ', JSON.parse(error));
+            alert(
+                'There was an issue with your payment! Please make sure you use the provided credit card.'
+            );
+        });
     };
 
     return (
