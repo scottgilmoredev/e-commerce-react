de72c4ed0c6be275de18109172dc27b492ae021a
Scott Gilmore <scott@plsol.com>
2019-10-15 14:16:41 -0400

    Add documentation

--- client/src/components/spinner/spinner.component.jsx
+++ client/src/components/spinner/spinner.component.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 // Styles
 import { SpinnerContainer, SpinnerOverlay } from './spinner.styles';
 
+/**
+ * Display the loading spinner.
+ */
 const Spinner = () => (
     <SpinnerOverlay>
         <SpinnerContainer />
