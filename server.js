f124dd4e594c5c3ab594fdd0a42edee9ef3a95d9
Scott Gilmore <scott@plsol.com>
2019-10-15 16:16:34 -0400

    Add compression library for gzipping on herkoku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
+const compression = require('compression');
+const cors = require('cors');
+const express = require('express');
 const path = require('path');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
@@ -12,6 +13,8 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(compression());
 
 app.use(cors());
 
