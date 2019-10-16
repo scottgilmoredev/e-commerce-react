14a4a47c0e3802cf9143c91b06dd52d1e11e5b28
Scott Gilmore <scott@plsol.com>
2019-10-16 11:29:14 -0400

    Make app PWA compliant

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const compression = require('compression');
 const cors = require('cors');
+const enforce = require('express-sslify');
 const express = require('express');
 const path = require('path');
 
@@ -11,12 +12,18 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Body Parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Compression
 app.use(compression());
 
+// CORS
 app.use(cors());
+
+// Enforce
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
@@ -29,6 +36,10 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(port, error => {
   if (error) throw error;
   console.log('Server running on port ' + port);
+});
+
+app.get('/service-worker.js', (requeest, response) => {
+    response.send(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 });
 
 app.post('/payment', (request, response) => {
