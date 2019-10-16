091474971ab678def25574d396d5811c777f503a
Scott Gilmore <scott@plsol.com>
2019-10-16 13:13:05 -0400

    Move compression and enforce to the production block

--- server.js
+++ server.js
@@ -16,16 +16,12 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Compression
-app.use(compression());
-
 // CORS
 app.use(cors());
 
-// Enforce
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
-
 if (process.env.NODE_ENV === 'production') {
+    app.use(compression());
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', function(request, response) {
