f651a75ed389e3f7e0ca3b10bd17370cbe5d51a3
Scott Gilmore <scott@plsol.com>
2019-10-18 17:15:51 -0400

    Implement SignInAndSignUp page tests

---
+++ client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.spec.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+// Component
+import SignInAndSignUpPage from './sign-in-and-sign-up.component';
+
+// Render the component
+it('should render SignInAndSignUpPage component', () => {
+  expect(shallow(<SignInAndSignUpPage />)).toMatchSnapshot();
+});
