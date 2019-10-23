ce5d3ed06aee9f7b325edfafa3f4b30a6c39b649
Scott Gilmore <scott@plsol.com>
2019-10-23 10:18:05 -0400

    Clean up footer layout

--- client/src/components/footer/footer.component.jsx
+++ client/src/components/footer/footer.component.jsx
@@ -5,8 +5,10 @@ import { FooterContainer, LinkText } from './footer.styles';
 
 const Footer = () => (
     <FooterContainer>
-        Feel free to view the source code on
-        <LinkText href="https://github.com/awesomescott/harrys-hoods">&nbsp;GitHub.</LinkText>
+        <p>
+            Feel free to view the source code on
+            <LinkText href="https://github.com/awesomescott/harrys-hoods">&nbsp;GitHub.</LinkText>
+        </p>
     </FooterContainer>
 );
 
