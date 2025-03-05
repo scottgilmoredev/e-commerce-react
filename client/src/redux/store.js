11e71fdc920ef5947708da37294ad819b6b7be94
Scott Gilmore <scott@plsol.com>
2025-03-05 16:23:23 -0500

    Migrate off deprecated Redux createStore to Redux Toolkit

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import { persistStore } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
@@ -7,14 +7,22 @@ import rootReducer from './root-reducer';
 import rootSaga from './root-saga';
 
 const sagaMiddleware = createSagaMiddleware();
-const middlewares = [
-    sagaMiddleware,
-];
 
-if (process.env.NODE_ENV !== 'production') {
-    middlewares.push(logger);
-}
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => {
+    const middleware = getDefaultMiddleware({
+      thunk: false,
+      serializableCheck: false,
+    }).concat(sagaMiddleware);
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+    if (process.env.NODE_ENV !== 'production') {
+      middleware.push(logger);
+    }
+
+    return middleware;
+  },
+});
+
 sagaMiddleware.run(rootSaga);
 export const persistor = persistStore(store);
